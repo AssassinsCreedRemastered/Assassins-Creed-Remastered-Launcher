b191759f931bb98cf4551bd397618256c5b01c78
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2023-07-05 08:41:45 +0200

    Design Update 2

--- ACRemasteredLauncher/MainWindow.xaml.cs
+++ ACRemasteredLauncher/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -26,18 +27,13 @@ namespace ACRemasteredLauncher
     /// </summary>
     public partial class MainWindow : Window
     {
-
         public MainWindow()
         {
             InitializeComponent();
             GC.Collect();
         }
+
         //Events
-        private void Window_Drag(object sender, MouseButtonEventArgs e)
-        {
-
-        }
-
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
             PageViewer.Source = new Uri("Pages/Default Page.xaml", UriKind.Relative);
@@ -45,17 +41,59 @@ namespace ACRemasteredLauncher
 
         private void OptionsButton_Click(object sender, RoutedEventArgs e)
         {
-
+            PageViewer.Source = new Uri("Pages/Options Page.xaml", UriKind.Relative);
         }
 
+        // Exit the program
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
             Environment.Exit(0);
         }
 
+        // Open the Credits Page
         private void Credits_Click(object sender, RoutedEventArgs e)
         {
             PageViewer.Source = new Uri("Pages/Credits Page.xaml", UriKind.Relative);
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService navigationService = PageViewer.NavigationService;
+            if (navigationService.CanGoBack)
+            {
+                navigationService.GoBack();
+            }
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void PageViewer_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            Default_Page dp = new Default_Page();
+            switch (e.Uri.ToString())
+            {
+                case string s when s.EndsWith("Default%20Page.xaml"):
+                    Console.WriteLine("Default Page");
+                    break;
+                case string s when s.EndsWith("Credits%20Page.xaml"):
+                    Console.WriteLine("Credits Page");
+                    //dp.PageImageSource = new BitmapImage(new Uri("../Assets/background.png"));
+                    Console.WriteLine(dp.PageImageSource);
+                    dp.Test2();
+                    break;
+                case string s when s.EndsWith("Options%20Page.xaml"):
+                    Console.WriteLine("Options Page");
+                    dp.Test2();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
---
+++ ACRemasteredLauncher/Pages/Options Page.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ACRemasteredLauncher.Pages
+{
+    /// <summary>
+    /// Interaction logic for Options_Page.xaml
+    /// </summary>
+    public partial class Options_Page : Page
+    {
+        public Options_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
